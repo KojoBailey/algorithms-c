7da5fc7015158db340cc442321b902d86102a94a
Kojo Bailey <kojomolojo@gmail.com>
2026-02-27 15:42:58 +0000

    Add bubble sort in C

---
+++ bubble-sort/main.c
@@ -0,0 +1,56 @@
+%:include <stdio.h>
+%:include <stdbool.h>
+%:include <stdlib.h>
+%:include <time.h>
+
+void swap(int* a, int* b)
+{
+	if (a == b) return;
+	*a ^= *b;
+	*b ^= *a;
+	*a ^= *b;
+}
+
+void print_arr(int* arr, size_t length)
+{
+	for (size_t i = 0; i < length; i++) {
+		printf("%d ", arr[i]);
+	}
+}
+
+void bubble_sort(int* arr, size_t length)
+{
+	size_t i, j;
+	for (i = 0; i < length - 1; i++) {
+		bool was_swap_performed = false;
+
+		for (j = 0; j < length - i - 1; j++) {
+			if (arr[j] > arr[j+1]) {
+				swap(&arr[j], &arr[j+1]);
+				was_swap_performed = true;
+			}
+		}
+
+		if (!was_swap_performed) break;
+	}
+}
+
+int main(void)
+{
+	srand(time(NULL));
+	int data[1000];
+	size_t data_length = sizeof(data) / sizeof(data[0]);
+
+	for (size_t i = 0; i < data_length; i++) {
+		data[i] = rand();
+		printf("%d ", data[i]);
+	}
+
+	puts("\nPerforming bubble sort...");
+	bubble_sort(data, data_length);
+
+
+	for (size_t i = 0; i < data_length; i++) {
+		printf("%d ", data[i]);
+	}
+}
